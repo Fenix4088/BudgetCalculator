fc8d639a827d9eedfe6cced328a7632ea740e8e3
Fenix <fenix4088@gmail.com>
2020-05-15 19:04:21 +0200

    Format numbers in table

--- js/view.js
+++ js/view.js
@@ -25,6 +25,69 @@ let viewController = (function() {
             }
     }
 
+    // Ф-я для форматирования чисел
+    // Принемает число и тип этого числа
+    function formatNumber(num, type) {
+        let numSplit, int, dec, resultNumber;
+        let newInt = "";
+
+        // Уюираем знак минус у отрицательных чисел
+        num = Math.abs(num); //Math.abs(-10) = 10
+
+        // Приводим к двум цифрам после точки
+        num = num.toFixed(2); // 2.45678.toFixed(2) = 2.46 / 2.toFixed(2) = 2.00
+        // Разделяем число на две части на 123000 => 123,000.00/ 123456789 => 123,456,789.00
+        numSplit = num.split("."); // 45.78 => [45, 78]
+        int = numSplit[0]; // целая часть
+        dec = numSplit[1]; // десятичная часть
+
+        // Раставляем запятые
+        // Исходя из длинны числа мы делим его на части по три цифры
+        // Начиная с правой стороны проставляем запятые после каждого третьего числа
+        // Если длинна номера больше чем три цифры значит нужно ставить запятые
+        if(int.length > 3) {
+            console.log("formatNumber -> int.length", int.length)
+            // Цикл который определяет сколько запятых необходимо поставить
+            for(let i = 0; i < int.length / 3; i++ ) {
+                // Формируем новую строку с номером
+                newInt = 
+                // Добавляем запятую каждые три числа
+                "," +
+                // Вырезанный кусок из исходной строки 
+                int.substring(int.length - 3 * (i + 1), int.length - 3 * i) + 
+                // Конец строки первая часть
+                newInt;
+                console.log("formatNumber -> newInt", newInt)
+
+            }
+            console.log("formatNumber -> newInt", newInt)
+
+            // Убираем запятую в начале, если она есть
+            // Если у строки первый символ равен запятой то верни нам строку начиная со второго символа
+            if(newInt[0] === ",") {
+                newInt = newInt.substring(1);
+                console.log(newInt);
+            }
+
+        } else if(int === "0") { 
+            // если исходное число равно нулю, то в новую строку записываем ноль
+            newInt = "0";
+        } else {
+            newInt = int;
+        }
+        
+        resultNumber = newInt + "." + dec;
+
+        if(type === "exp") {
+            resultNumber = "- " + resultNumber;
+        } else if (type === "inc") {
+            resultNumber = "+ " + resultNumber;
+
+        }
+
+        return resultNumber;
+    }
+
     // Метод с помощью которого мы будем вставлять "запись" в разметку
     // Принемает в себя обьект записи и тип(куда она относится в рассход или доход)
     function renderListItem(obj, type) {
@@ -67,7 +130,7 @@ let viewController = (function() {
         // Работаем с переменной newHtml переписывая ее раз за разом
         let newHtml = html.replace("%id%", obj.id);
         newHtml = newHtml.replace("%description%", obj.description);
-        newHtml = newHtml.replace("%value%", obj.value);
+        newHtml = newHtml.replace("%value%", formatNumber(obj.value, type));
 
 
         document.querySelector(containerElement).insertAdjacentHTML("beforeend", newHtml);
@@ -89,10 +152,17 @@ let viewController = (function() {
 
     // Ф-я для отображения подсчитанных рассходов доходов и процентов в приложении
     function displayBudget(obj) {
+        
+        let type;
+        if (obj.budget > 0) {
+            type = "inc";
+        } else {
+            type = "exp";
+        }
 
-        document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
-        document.querySelector(DOMstrings.incomeLabel).textContent = "+ " + obj.totalInc;
-        document.querySelector(DOMstrings.expanceLabel).textContent = "- " + obj.totalExp;
+        document.querySelector(DOMstrings.budgetLabel).textContent = formatNumber(obj.budget, type);
+        document.querySelector(DOMstrings.incomeLabel).textContent = formatNumber(obj.totalInc, "inc");
+        document.querySelector(DOMstrings.expanceLabel).textContent = formatNumber(obj.totalExp, "exp");
 
         if(obj.percentage > 0) {
             document.querySelector(DOMstrings.expancePercentLabel).textContent = obj.percentage + "%";
@@ -116,7 +186,7 @@ let viewController = (function() {
         // item => [exp-id, 26%]
         // Находим блок с процентами внутри текущей записи
         let el = document.getElementById(`exp-${item[0]}`).querySelector('.item__percent'); // li нашли затем проценты внутри li
-        console.log("updateItemsPercentages -> el", el);
+
 
         // Делаем проверку если значение % = "-1" когда нет доходов
         if(item[1] >= 0) {
